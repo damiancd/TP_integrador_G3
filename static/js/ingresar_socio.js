80fc8b7c56bd9f342d53c43706e3ed08d780b91e
damiancd <135079931+damiancd@users.noreply.github.com>
2024-07-12 14:26:43 -0300

    Update ingresar_socio.js

--- static/js/ingresar_socio.js
+++ static/js/ingresar_socio.js
@@ -1,3 +1,56 @@
+function ValidaryGuardar() {
+    let nombre_ingresado = document.getElementById("nombre").value //input
+    let apellido_ingresado = document.getElementById("apellido").value 
+    let dni_ingresado = document.getElementById("dni").value 
+    let correo_ingresado = document.getElementById("correo").value
+    let actividad_ingresada = document.getElementById("actividad").value  
+
+    let nya_Regex = /^[a-zA-Z\s]+$/;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (nombre_ingresado === "") {
+        alert("Por favor, ingresa tu nombre.");
+        return false;
+    }
+    
+    if (!nya_Regex.test(nombre_ingresado)) {
+    alert("El nombre no debe contener números ni caracteres especiales.");
+    return false;}
+    
+    if (apellido_ingresado === "") {
+        alert("Por favor, ingresa tu apellido.");
+        return false;}
+
+    if (!nya_Regex.test(apellido_ingresado)) {
+    alert("El apellido no debe contener números ni caracteres especiales.");
+    return false;}
+
+    if (dni_ingresado === "") {
+        alert("Por favor, ingresa tu DNI.");
+        return false;}
+
+    if (!/^\d+$/.test(dni_ingresado)) {
+        alert("El DNI debe ser numérico.");
+        return false;}
+
+    if (correo_ingresado === "") {
+        alert("Por favor, ingresa tu correo electrónico.");
+        return false;    }
+   
+    if (!emailRegex.test(correo_ingresado)) {
+        alert("Por favor, ingresa un correo electrónico válido.");
+        return false;        }
+
+    if(actividad_ingresada===""){
+        error.textContent = "Por favor seleccione una actividad."
+        return false}
+
+    guardar();
+}
+
+
+
+
 function guardar() {
     let nombre_ingresado = document.getElementById("nombre").value //input
     let apellido_ingresado = document.getElementById("apellido").value 
@@ -7,7 +60,7 @@ function guardar() {
     let actividad_ingresada = document.getElementById("actividad").value  
 
 
-    console.log(nombre_ingresado,apellido_ingresado,dni_ingresado,imagen_ingresada,correo_ingresado,actividad_ingresada);
+    //console.log(nombre_ingresado,apellido_ingresado,dni_ingresado,imagen_ingresada,correo_ingresado,actividad_ingresada);
     // Se arma el objeto de js 
     let datos = {
         nombre: nombre_ingresado,
