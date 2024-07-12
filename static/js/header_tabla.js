f2a9cd85e4de8b0f52fa51baea7cfce2b6b2708d
damiancd <135079931+damiancd@users.noreply.github.com>
2024-07-12 12:27:05 -0300

    Update header_tabla.js

--- static/js/header_tabla.js
+++ static/js/header_tabla.js
@@ -1,7 +1,13 @@
 document.getElementById("header_tabla").innerHTML= `
+<div class="en-linea">
+<h1><i class="fas fa-running"></i> CLUB SOCIAL Y DEPORTIVO</h1>
+
+<nav class="en-linea">
+<br>
 <nav class="menu">
             <a href="/tabla_socios.html">Listado de Socios</a>
             <a href="/templates/ingresar_socio.html">ABM Socios</a>
+            <a href="/index.html">Salir</a>
 </nav>
 
 `
